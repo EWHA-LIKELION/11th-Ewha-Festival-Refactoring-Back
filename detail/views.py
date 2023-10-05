50c1a194a1a985b7051d2f36e14ac8d45769b7c6
yewon <shimyewon@ewhain.net>
2023-10-06 01:15:30 +0900

    [fix]좋아요 get 추가

--- detail/views.py
+++ detail/views.py
@@ -18,7 +18,26 @@ class EventDetailView(views.APIView):
 # Create your views here.
 
 
-class MenuLikeViewv(views.APIView):
+class MenuLikeView(views.APIView):
+    def get(self, request, menu_id):
+        try:
+            event = Event.objects.get(pk=menu_id)
+        except Event.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        is_liked = event.like.filter(id=request.user.id).exists()
+
+        response_data = {
+            "message": "메뉴 좋아요 상태 조회 성공",
+            "data": {
+                "menu": menu_id,
+                "is_liked": is_liked,
+            }
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
     def patch(self, request, menu_id):
         try:
             menu = Menu.objects.get(pk=menu_id)
@@ -42,8 +61,29 @@ class MenuLikeViewv(views.APIView):
         }
 
         return Response(response_data, status=status.HTTP_200_OK)
+
     
 class EventLikeView(views.APIView):
+    def get(self, request, event_id):
+        try:
+            event = Event.objects.get(pk=event_id)
+        except Event.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # 사용자의 좋아요 상태 확인
+        is_liked = event.like.filter(id=request.user.id).exists()
+
+        response_data = {
+            "message": "이벤트 좋아요 상태 조회 성공",
+            "data": {
+                "event": event_id,
+                "is_liked": is_liked,
+            }
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
     def patch(self, request, event_id):
         try:
             event = Event.objects.get(pk=event_id)
